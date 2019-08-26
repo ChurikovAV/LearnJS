99698d65116a62e97eb8c50e15d45a688a4e0a70
Anatoliy <anatoliy.churikov@gmail.com>
2019-08-26 14:23:50 +0700

    Practic lesson_4, not home work

--- lesson_4/practic.js
+++ lesson_4/practic.js
@@ -18,73 +18,53 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
-};
-
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-      let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-          b = prompt ("Во сколько обойдется?", "");
-
-      if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-
-          console.log ("done");
-
-          appData.expenses[a] = b;
-      } else {                            
-          console.log ("bad result");
-          i--;
-      }
-
-    };
-}
-
-chooseExpenses();
-
-appData.moneyPerDay = (appData.budget / 30).toFixed();
-
-function detectLevel () {
-    if (appData.moneyPerDay < 100) {
-        console.log ("Это минимальный уровень достатка!");
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        console.log ("Это средний уровень достатка!");
-    } else if (appData.moneyPerDay > 2000) {
-        console.log ("Это высокий уровень достатка!");
-    } else {
-        console.log ("Произошла ошибка");
-    }
-}
-
-detectLevel();
-
-function detectDayBudget() {
-    appData.moneyPerDay = appData.budget / 30;
-    alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
-}
-
-detectDayBudget();
-
-
-function checkServings() {
-    if (appData.savings == true) {
-        let save = +prompt("Какова сумма накоплений"),
+    savings: true,
+    chooseExpenses: function() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+                b = prompt ("Во сколько обойдется?", "");
+            if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+                console.log ("done");
+                appData.expenses[a] = b;
+            } else {                            
+                console.log ("bad result");
+                i--;
+            }
+        };
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = appData.budget / 30;
+        alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+    },
+    detectLevel: function() {
+        if (appData.moneyPerDay < 100) {
+            console.log ("Это минимальный уровень достатка!");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log ("Это средний уровень достатка!");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log ("Это высокий уровень достатка!");
+        } else {
+            console.log ("Произошла ошибка");
+        }
+    },
+    checkServings: function() {
+        if (appData.savings == true) {
+            let save = +prompt("Какова сумма накоплений"),
             percent = +prompt("Под какой годовой процент");
-
-        appData.monthIncome = save/100/12*percent;
-        alert("Доход в месяц с Вашего депозита: " + appData.monthIncome);
+            appData.monthIncome = save/100/12*percent;
+            alert("Доход в месяц с Вашего депозита: " + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function() {
+        for(let i = 1; i < 3; i++) {
+            let opt = prompt ("Статья необязательных расходов?", "");
+            appData.optionalExpenses[i] = opt;
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt("Что принесёт дополнительный доход (Перечислите через запятую)", "");
+        appData.income = items.split(', ');
+        appData.income.push(prompt("Может что-то ещё?"));
+        appData.income.sort();
     }
-}
-
-checkServings();
-
-
-let i = 1;
-let optionalExpenses = {};
-function chooseOptExpenses () {
-    while (i < 4) {
-       optionalExpenses[i] = prompt ("Статья необязательных расходов?", "");
-       i++;
-    }
-}
-
-chooseOptExpenses ();
+};
